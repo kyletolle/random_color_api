776ce75721ce8b316031f7e75e6532434bfb24a9
Kyle Tolle <kyle@nullsix.com>
2022-06-29 18:27:08 +0000

    Pad the hex colors with leading 0s when needed

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { crypto } from "https://deno.land/std@0.140.0/crypto/mod.ts";
 
+const toHex = decimal => decimal.toString(16).toUpperCase().padStart(2, '0')
+
 serve((_req) => {
     const [red, green, blue] = crypto.getRandomValues(new Uint8Array(3));
-    const redHex = red.toString(16).toUpperCase();
-    const greenHex = green.toString(16).toUpperCase();
-    const blueHex = blue.toString(16).toUpperCase();
+    const redHex = toHex(red);
+    const greenHex = toHex(green)
+    const blueHex = toHex(blue)
     const hexColor = `#${redHex}${greenHex}${blueHex}`;
 
     return new Response(hexColor, {
