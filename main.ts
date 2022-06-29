04f684b95cbe1bcdc32cd155e479549bd03e40bb
Kyle Tolle <kyle@nullsix.com>
2022-06-29 16:14:11 +0000

    Generate an actual random hex color

--- main.ts
+++ main.ts
@@ -2,8 +2,13 @@ import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { crypto } from "https://deno.land/std@0.140.0/crypto/mod.ts";
 
 serve((_req) => {
-    const randomValues = crypto.getRandomValues(new Int8Array(3));
-    return new Response(randomValues.toString(), {
+    const [red, green, blue] = crypto.getRandomValues(new Uint8Array(3));
+    const redHex = red.toString(16).toUpperCase();
+    const greenHex = green.toString(16).toUpperCase();
+    const blueHex = blue.toString(16).toUpperCase();
+    const hexColor = `#${redHex}${greenHex}${blueHex}`;
+
+    return new Response(hexColor, {
       headers: { "content-type": "text/plain" },
     });
   });
